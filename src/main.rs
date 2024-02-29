6dad7363dd1e7c94498fc6b0b05b4dd4aea9db9e
Seth Guimont <sethguimont@outlook.com>
2024-02-28 22:44:21 -0600

    experimenting with env variables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use thirtyfour::prelude::*;
+use dotenv::dotenv;
+
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
+    dotenv().ok();
     let mut caps = DesiredCapabilities::chrome();
+    //let email = std::env::var("EMAIL");
+    //let password = std::env::var("PASSSWORD");
     caps.add_chrome_arg("--enable-automation")?;
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
 
@@ -12,6 +17,16 @@ async fn main() -> WebDriverResult<()> {
     println!("Title = {}", driver.title().await?);
 
     driver.find(By::ClassName("amplify-image")).await?;
+
+    // Find element from element.
+    let email_input = driver.find(By::Id("amplify-id-:r4:")).await?;
+    let password_input = driver.find(By::Id("amplify-id-:r7:")).await?;
+    
+
+    // Type in email.
+    email_input.send_keys().await?;
+    // Type in password
+    password_input.send_keys().await?;
 
      // Always explicitly close the browser.
     driver.quit().await?;
